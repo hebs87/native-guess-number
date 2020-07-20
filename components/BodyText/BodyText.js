6edbbc18a624e0521f3a33fcb494846aa685f4af
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-20 21:14:42 +0100

    Created TitleText and BodyText components with set fonts to use in project

---
+++ components/BodyText/BodyText.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const BodyText = props => {
+  return (
+    <Text style={{...styles.title, ...props.style}}>
+      {props.children}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'OpenSans',
+  },
+});
+
+export default BodyText;
