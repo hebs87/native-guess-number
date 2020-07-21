149df4454d732791fe665e1f5b0fa8cf6d722925
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-21 15:02:29 +0100

    Rendered different button component based on platform in MainButton

--- components/MainButton/MainButton.js
+++ components/MainButton/MainButton.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, TouchableNativeFeedback, Platform} from 'react-native';
 import Colors from '../../theme/colors';
 
 const MainButton = props => {
+  let ButtonComponent = Platform.OS === 'android' && Platform.Version >= 21 ? TouchableNativeFeedback : TouchableOpacity;
+
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-      <View style={{...styles.button, ...props.style}}>
-        <Text style={{...styles.buttonText, ...props.style}}>
-          {props.children}
-        </Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+        <View style={{...styles.button, ...props.style}}>
+          <Text style={{...styles.buttonText, ...props.style}}>
+            {props.children}
+          </Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: 'hidden',
+  },
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
