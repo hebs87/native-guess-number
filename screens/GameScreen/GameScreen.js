a8317d9dc1f6a3ff82406fe14baab74ef83c0405
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-07-21 12:02:44 +0100

    Applied responsive styles to GameScreen

--- screens/GameScreen/GameScreen.js
+++ screens/GameScreen/GameScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useRef, useEffect} from 'react';
-import {StyleSheet, View, Alert, ScrollView} from 'react-native';
+import {StyleSheet, View, Alert, ScrollView, Dimensions} from 'react-native';
 import BodyText from "../../components/BodyText/BodyText";
 import NumberContainer from "../../components/NumberContainer/NumberContainer";
 import Card from "../../components/Card/Card";
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: '80%',
   },
   lowerButton: {
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '60%',
+    width: Dimensions.get('window').width > 350 ? '60%' : '80%',
   },
 });
 
